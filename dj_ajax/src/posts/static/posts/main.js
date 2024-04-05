ffd7aa8acd924cb82a9127b72a10521d5b21ebbf
Exploser <exploserbusiness@gmail.com>
2024-04-05 12:42:48 -0400

    Add loading spinner to posts/main

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -2,17 +2,18 @@ console.log("HELLO WORLD")
 
 const helloWorldBox = document.getElementById('hello-world')
 const postsBox = document.getElementById('posts-box')
+const spinnerBox = document.getElementById('spinner-box')
 
 helloWorldBox.innerHTML = 'hello world'
 
 $.ajax({
     type: 'GET',
     url: '/hello-world/',
-    success: function(response){
+    success: function (response) {
         console.log(response.text)
         helloWorldBox.textContent = response.text
     },
-    error: function(error){
+    error: function (error) {
         console.log('error', error)
     }
 })
@@ -20,17 +21,20 @@ $.ajax({
 $.ajax({
     type: 'GET',
     url: '/data/',
-    success: function(response){
+    success: function (response) {
         console.log(response)
         const data = response.data
-        console.log(data)
-        data.forEach(element => {
-            postsBox.innerHTML += `
+        setTimeout(() => {
+            spinnerBox.classList.add('not-visible')
+            console.log(data)
+            data.forEach(element => {
+                postsBox.innerHTML += `
                 ${element.title} - <b>${element.body}</b><br>
             `
-        });
+            });
+        }, 100);
     },
-    error: function(error){
+    error: function (error) {
         console.error(error);
     }
 })
