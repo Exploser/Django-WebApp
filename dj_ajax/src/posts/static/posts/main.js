20e647ce605360d8eac9992dda73d6b1eb2b8595
Exploser <exploserbusiness@gmail.com>
2024-04-05 11:39:47 -0400

    Fist ajax call for DOM manipulation

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -3,3 +3,15 @@ console.log("HELLO WORLD")
 const helloWorldBox = document.getElementById('hello-world')
 
 helloWorldBox.innerHTML = 'hello world'
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response){
+        console.log(response.text)
+        helloWorldBox.textContent = response.text
+    },
+    error: function(error){
+        console.log('error', error)
+    }
+})
--- dj_ajax/src/posts/views.py
+++ dj_ajax/src/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Posts
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -7,3 +8,6 @@ def post_list_and_create(request):
     qs = Posts.objects.all()
     return render(request, 'posts/main.html', {'qs':qs})
 
+
+def hello_world_view(request):
+    return JsonResponse({'text': 'HELLO WORLD'})
