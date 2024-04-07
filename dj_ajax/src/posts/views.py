520ed5cb3f87de92b10379af8e53a7b8c808ecf1
Exploser <exploserbusiness@gmail.com>
2024-04-06 21:39:33 -0400

    Implement feature for users to be able to add new posts

--- dj_ajax/src/posts/views.py
+++ dj_ajax/src/posts/views.py
@@ -3,55 +3,68 @@ from .models import Posts
 from django.http import JsonResponse
 from .forms import PostForm
 from profiles.models import Profile
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 
 def post_list_and_create(request):
-    form = PostForm(request.POST or None)
-    # qs = Posts.objects.all()
-
-    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-        if form.is_valid():
-            author = Profile.objects.get(user=request.user)
-            instance = form.save(commit=False)
-            instance.author = author
-            instance.save()
-    
-    context = {
-        'form': form,
-    }
-
-    return render(request, 'posts/main.html', context)
+    if request.method == 'POST':
+        form = PostForm(request.POST or None)
+        if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            if form.is_valid() and request.user.is_authenticated:
+                author_profile, created = Profile.objects.get_or_create(user=request.user)
+                instance = form.save(commit=False)
+                instance.author = author_profile
+                instance.save()
+                return JsonResponse({
+                    'title': instance.title,
+                    'body': instance.body,
+                    'author': instance.author.user.username,
+                    'id': instance.id})
+            else:
+                return JsonResponse({'error': 'Invalid form data or user not authenticated'}, status=400)
+    else:
+        form = PostForm()
+    return render(request, 'posts/main.html', {'form': form})
 
 
 def load_post_data_view(request, num_posts):
-    visible = 3
-    upper = num_posts
-    lower = upper - visible
-    size = Posts.objects.all().count()
-    qs = Posts.objects.all()
-    data = []
-    for obj in qs:
-        item = {
-            'id': obj.id,
-            'title': obj.title,
-            'body': obj.body,
-            'liked': True if request.user in obj.liked.all() else False,
-            'count': obj.like_count,
-            'author': obj.author.user.username
-        }
-        data.append(item)
-    return JsonResponse({'data':data[lower:upper], 'size': size})
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        visible = 3
+        upper = num_posts
+        lower = upper - visible
+        size = Posts.objects.all().count()
+        qs = Posts.objects.all()
+        data = []
+        for obj in qs:
+            item = {
+                'id': obj.id,
+                'title': obj.title,
+                'body': obj.body,
+                'liked': True if request.user in obj.liked.all() else False,
+                'count': obj.like_count,
+                'author': obj.author.user.username
+            }
+            data.append(item)
+        return JsonResponse({'data':data[lower:upper], 'size': size})
 
 def like_unlike_post(request):
     # if request.is_ajax():
     if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-        pk = request.POST.get('pk')
-        obj = Posts.objects.get(pk=pk)
-        if request.user in obj.liked.all():
-            liked = False
-            obj.liked.remove(request.user)
+        if request.method == 'POST':
+            pk = request.POST.get('pk')
+            obj = get_object_or_404(Posts, pk=pk)
+            if request.user.is_authenticated:  # Check if user is authenticated
+                if request.user in obj.liked.all():
+                    liked = False
+                    obj.liked.remove(request.user)
+                else:
+                    liked = True
+                    obj.liked.add(request.user)
+                return JsonResponse({'liked': liked, 'count': obj.like_count})
+            else:
+                return JsonResponse({'error': 'User not authenticated'})
         else:
-            liked = True
-            obj.liked.add(request.user)
-        return JsonResponse({'liked': liked, 'count': obj.like_count})
+            return JsonResponse({'error': 'Invalid request method'})
+
         
