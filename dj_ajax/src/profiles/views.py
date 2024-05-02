68e3a994bd515b884bfd87817762aefbb1b25d6b
Exploser <exploserbusiness@gmail.com>
2024-05-02 16:39:24 -0400

    Add profile details to profile view

--- dj_ajax/src/profiles/views.py
+++ dj_ajax/src/profiles/views.py
@@ -8,12 +8,12 @@ from django.http import JsonResponse
 def my_profile_view(request):
     obj = Profile.objects.get(user=request.user)
     form = ProfileForm(request.POST or None, request.FILES or None, instance=obj)
-    if request.is_ajax():
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
         if form.is_valid():
             instance = form.save()
             return JsonResponse({
                 'bio': instance.bio,
-                'avatar': instance.avatar.url,
+                'avatar': instance.avatar.url if instance.avatar else None,
                 'user': instance.user.username
             })
     context = {
